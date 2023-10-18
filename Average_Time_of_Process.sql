8ee827a670cf37c3e037959e03e230cfac94f642
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-18 11:23:45 +0530

    Add files via upload

---
+++ Average_Time_of_Process.sql
@@ -0,0 +1,6 @@
+select a1.machine_id, round(avg(a2.timestamp-a1.timestamp), 3) as processing_time 
+from Activity a1
+join Activity a2 
+on a1.machine_id=a2.machine_id and a1.process_id=a2.process_id
+and a1.activity_type='start' and a2.activity_type='end'
+group by a1.machine_id
