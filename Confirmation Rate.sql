1c6e431ec132e81b149c7769c754c1cdab5251a5
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-18 11:25:10 +0530

    Add files via upload

---
+++ Confirmation Rate.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select s.user_id, round(avg(if(c.action="confirmed",1,0)),2) as confirmation_rate
+from Signups as s left join Confirmations as c on s.user_id= c.user_id group by user_id;
+
