46c011cda5938a0467e04296de50eb35020a1977
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-23 22:22:43 +0530

    Add files via upload

---
+++ Consecutive Numbers.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
