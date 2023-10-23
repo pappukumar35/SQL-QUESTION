66818eeffd5969f47f3a5ba33ef186cea7ded7c6
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-23 22:21:56 +0530

    Add files via upload

---
+++ Count Salary Categories.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT "Low Salary" AS category,
+       sum(income < 20000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "Average Salary" AS category,
+       sum(income BETWEEN 20000 AND 50000) AS accounts_count
+  FROM Accounts
+
+UNION
+
+SELECT "High Salary" AS category,
+       sum(income > 50000) AS accounts_count
+  FROM Accounts;
