794b458d17feff3e1bad5d538de52df621ecbaa9
Pappu Kumar <119037956+pappukumar35@users.noreply.github.com>
2023-10-23 22:20:57 +0530

    Add files via upload

---
+++ asked_step_by_step.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT activity_date AS day, COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE (activity_date > "2019-06-27" AND activity_date <= "2019-07-27")
+GROUP BY activity_date;
